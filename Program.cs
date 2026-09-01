d6df701137a2a2863d97a6618704a8dbec7b93f6
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-01 21:33:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.Write("how ");
+Console.Write("are ");
+Console.Write("YOU!");
+
+Console.WriteLine("Hello World");
+Console.WriteLine(123456);
+Console.WriteLine("5+5");
+Console.WriteLine(5 + 5);
+
